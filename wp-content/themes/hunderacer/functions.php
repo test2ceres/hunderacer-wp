cba898462101785547c3c7561bdfa44f82384687
Tran Pham <tran.pham@ceres.com.vn>
2017-12-01 11:57:31 +0700

    add css main navigation

--- wp-content/themes/hunderacer/functions.php
+++ wp-content/themes/hunderacer/functions.php
@@ -7,3 +7,8 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+add_action('admin_head', 'admin_hunderacer_custom_fonts');
+function admin_hunderacer_custom_fonts() {
+    wp_enqueue_style('admin_styles', get_template_directory_uri(). '/css/style.css');
+}
